c1f142f79493753eecf73c28dcaf256f7a95356f
Matthew Miller <mnmiller1@me.com>
2016-08-18 15:42:25 +1000

    Added the help screen and info bars

--- pong.h
+++ pong.h
@@ -1,6 +1,33 @@
 #pragma once
 
-const int DELAY = 1000 / 60;
+// Program management variables
+const int FPS = 60;
+const int DELAY = 1000 / FPS;
 bool running = true;
+bool display_help_screen = true;
+
+// Level management variables.
+int level = 0;
+int score = 0;
+int lives = 10;
+int game_time = 0;
+const int LEVEL_COUNT = 4;
+
+// Help Screen
+const char * help_screen_text[] = {
+        "Pong",
+        "Created by Madeline Miller - n9342401",
+        "",
+        "Controls",
+        "L for next level",
+        "H for help",
+        "",
+        "Press any key to exit"
+
+};
 
 void process();
+
+void start_level(int new_level);
+
+int string_length(char * str);
